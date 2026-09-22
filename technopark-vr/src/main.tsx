21bd2f49e91fb639e7d87bac937175d42efa9d1c
STEP3DLab <projects.step3d@gmail.com>
2026-09-22 21:03:56 +0300

    Register offline fallback service worker

--- technopark-vr/src/main.tsx
+++ technopark-vr/src/main.tsx
@@ -3,3 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import Experience from './Experience';
 createRoot(document.getElementById('root')!).render((location.search.includes('gallery=1') ? <App/> : <Experience/>));
+if('serviceWorker' in navigator&&(location.protocol==='https:'||location.hostname==='localhost'||location.hostname==='127.0.0.1')){
+  navigator.serviceWorker.register('./sw.js').catch(()=>{});
+}
